9120835db23264caa1a4a3595ec80008bf106136
Nick Van Haver <nvhaver@gmail.com>
2016-11-20 00:01:08 +0100

    Project scaffolding
    Generated the project scaffolding using Visual Studio 2015 (MVC project
    without authentication).

---
+++ WebApplicationBase/WebApplicationBase/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationBase
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WebApplicationBase/WebApplicationBase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationBase.MvcApplication" Language="C#" %>
